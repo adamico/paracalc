73e4324c4ef8db536321000488f6710992cd5e15
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-03 17:38:10 +0200

    added root route, removed stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Paracalc::Application.routes.draw do
+  root :to => 'high_voltage/pages#show', :id => 'home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
